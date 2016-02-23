3750663d6e7acdee4811286462d6dc5bcf46c51c
James Teitsworth <james.teitsworth@outlook.com>
2016-02-23 15:22:08 -0600

    Refactor create method.

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,27 +3,31 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
   @@data = []
+  @@path = File.dirname(__FILE__) + "/../data/data.csv"
 
   def self.create(options = {})
-    csv_array = setup_row(options)
-    CSV.open(@@data_path, "ab") do |csv|
-      csv << csv_array
-    end
-    @@data << Product.new(options)
-    @@data.last
-  end
-
-  def self.setup_row(options)
-    csv_array = []
-    options.map do |_k, v|
-      csv_array << v
-    end
-    csv_array
+    new_product = Product.new(options)
+    @@data << new_product
+    insert_data(to_csv_array(new_product))
+    new_product
   end
 
   def self.all
     @@data
+  end
+
+  def self.first
+    @@data.first
+  end
+
+  def self.insert_data(data_array)
+    CSV.open(@@path, "ab") do |csv|
+      csv << data_array
+    end
+  end
+
+  def self.to_csv_array(database_obj)
+    [database_obj.id, database_obj.brand, database_obj.name, database_obj.price]
   end
 end

c9419948e3e76f7f1089eef68f0c8bfde00fe714
James Teitsworth <james.teitsworth@outlook.com>
2016-03-01 00:23:25 -0600

    Change all method to be in sync with the database file.

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,34 +3,37 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  @@data = []
-  @@path = File.dirname(__FILE__) + "/../data/data.csv"
+  PATH = File.dirname(__FILE__) + "/../data/data.csv"
   Module.create_finder_methods(:brand, :name)
 
   def self.create(options = {})
     new_product = Product.new(options)
     in_db = object_in_db?(new_product)
-    @@data << new_product unless in_db
     insert_data(to_csv_array(new_product)) unless in_db
     new_product
   end
 
   def self.all
-    @@data
+    data = CSV.read(PATH).drop(1)
+    products = []
+    data.each do |line|
+      products << array_to_objects(line)
+    end
+    products
   end
 
   def self.first(n = 0)
-    return @@data.first if n == 0
-    @@data.first(n)
+    return all.first if n == 0
+    all.first(n)
   end
 
   def self.last(n = 0)
-    return @@data.last if n == 0
-    @@data.last(n)
+    return all.last if n == 0
+    all.last(n)
   end
 
   def self.find(id)
-    found = @@data.find do |record|
+    found = all.find do |record|
       record.id == id
     end
     check_product_exists(found)
@@ -65,7 +68,7 @@ class Udacidata
   end
 
   def self.generate_post_delete_array(product)
-    database = CSV.read(@@path)
+    database = CSV.read(PATH)
     new_database = database.select do |record|
       record[0].to_i != product.id
     end
@@ -73,7 +76,7 @@ class Udacidata
   end
 
   def self.overwrite_file(csv_array)
-    CSV.open(@@path, "wb") do |csv|
+    CSV.open(PATH, "wb") do |csv|
       csv_array.each do |record|
         csv << record
       end
@@ -81,7 +84,7 @@ class Udacidata
   end
 
   def self.insert_data(data_array)
-    CSV.open(@@path, "ab") do |csv|
+    CSV.open(PATH, "ab") do |csv|
       csv << data_array
     end
   end
@@ -90,9 +93,12 @@ class Udacidata
     [data_object.id, data_object.brand, data_object.name, data_object.price]
   end
 
+  def self.array_to_objects(csv_array)
+    create(id: csv_array[0], brand: csv_array[1], name: csv_array[2], price: csv_array[3])
+  end
+
   def self.object_in_db?(db_object)
-    # TODO check other fields not id.
-    db = CSV.read(@@path)
+    db = CSV.read(PATH)
     existing_record = db.find do |record|
       record[0].to_i == db_object.id
     end
